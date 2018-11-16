854c6ee7a7886f6c9356d71c316319925561bcac
Monferd Collin <mcollin@Monferds-MacBook-Pro.local>
2018-11-16 17:27:18 -0500

    merging to master

--- client/app.js
+++ client/app.js
@@ -1,55 +1,22 @@
 import React, {Component, Fragment} from 'react'
 import {Navbar} from './components'
 import Routes from './routes'
-<<<<<<< HEAD
-<<<<<<< HEAD
-import {CssBaseline} from '@material-ui/core/'
-
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <CssBaseline />
-        {/* <Navbar /> */}
-        <Routes />
-      </Fragment>
-    )
-  }
-=======
 import NYCNeighborhoods from './components/NYCneighborhoods'
+import {CssBaseline} from '@material-ui/core/'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
+    <Fragment>
+      <CssBaseline />
+      {/* <Navbar /> */}
       <Routes />
       <div className="App">
-        <svg width="650" height="720">
-          <NYCNeighborhoods width={650} height={720} />
+        <svg width="960" height="720">
+          <NYCNeighborhoods width={960} height={720} />
         </svg>
       </div>
-    </div>
+    </Fragment>
   )
->>>>>>> origin/master
-=======
-import NYCNeighborhoods from './components/NYCneighborhoods'
-import {CssBaseline} from '@material-ui/core/'
-
-const App = () => {
-  
-    return (
-      <Fragment>
-        <CssBaseline />
-        {/* <Navbar /> */}
-        <Routes />
-        <div className="App">
-          <svg width="960" height="720">
-            <NYCNeighborhoods width={960} height={720} />
-          </svg>
-        </div>
-      </Fragment>
-    )
->>>>>>> d336129cf1cca2f2f538c018a907e6d0f7b79eb0
 }
 
 export default App
