c0c29adab99b6a1323b64b2d0c8f730c5b5bdafd
Kirsten Chong <kirstch7@gmail.com>
2018-11-18 17:35:58 -0500

    Added fetching of map to the redux thunk

--- client/store/map.js
+++ client/store/map.js
@@ -21,8 +21,10 @@ const getMap = mapData => ({type: GET_MAP, mapData})
 
 export const getMapData = () => dispatch => {
   try {
-    let mapData = {neighborhood: 'East Village'}
-    dispatch(getMap(mapData))
+    d3.json(
+      ' http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson',
+      mapData => dispatch(getMap(mapData))
+    )
   } catch (error) {
     console.error(error)
   }
