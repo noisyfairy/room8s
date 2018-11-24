421e965ff7f582d23225174d24e75c778870ccc4
Kirsten Chong <kirstch7@gmail.com>
2018-11-23 20:20:39 -0500

    Added a function to group neighborhood by score

--- client/components/MapQuestionnaireAnswer.js
+++ client/components/MapQuestionnaireAnswer.js
@@ -3,6 +3,31 @@ import {connect} from 'react-redux'
 import history from '../history'
 
 class MapQuestionnaireAnswer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      liveHere: []
+    }
+  }
+
+  componentDidMount() {
+    let neighborhoods = this.props.mapData.features
+    console.log(`NEIGHBORHOODS HERE: `, neighborhoods)
+    let mapScore = {}
+    for (let neighborhood of neighborhoods) {
+      console.log(neighborhood.properties.score)
+      if (mapScore[neighborhood.properties.score]) {
+        mapScore[neighborhood.properties.score].push(
+          neighborhood.properties.neighborhood
+        )
+      } else {
+        mapScore[neighborhood.properties.score] = [
+          neighborhood.properties.neighborhood
+        ]
+      }
+    }
+  }
+
   render() {
     return <div>"oh hai, looks like you should live here!"</div>
   }
